30103ff9d6ec6c75d5f514b190d0a0ba5501b157
AlexandreDuff <alexandredufresne18@hotmail.com>
2016-10-15 20:08:53 -0400

    Add files via upload

---
+++ Game1.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace Teka
+{
+
+    // ----------------------------------------------------------------------------------INTRO------------------------------------------------------------------------------------------------
+    // Alexandre Dufresne et Samuel Joyal
+    // Projet MonoGame
+    // TEKA
+    // "The enchanted kingdom of Astria"
+    //-----------------------------------------------------------------------------------INTRO------------------------------------------------------------------------------------------------
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        KeyboardState keys = new KeyboardState();
+        KeyboardState previousKeys = new KeyboardState();
+        Rectangle fenetre;
+        GameObjectAnime persoPrincipal;
+        bool intro = true;
+        GameObject mapIntro;
+        bool battle = false;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 1000;  
+            graphics.PreferredBackBufferHeight = 1000;   
+            graphics.ApplyChanges();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            persoPrincipal = new GameObjectAnime();
+            persoPrincipal.estVivant = true;  //seulement pour l instant
+            persoPrincipal.vitesse = 0;   //c est la map qui bouge
+            persoPrincipal.sprite = Content.Load<Texture2D>("SpriteSheet.png"); // ajouter image de base
+            persoPrincipal.position = new Rectangle(0, 0, 50, 72);
+            persoPrincipal.direction = Vector2.Zero;
+            persoPrincipal.etat = GameObjectAnime.Etat.AttenteBas;
+            persoPrincipal.position.Offset(fenetre.Center.X + persoPrincipal.sprite.Width/2, fenetre.Center.Y + persoPrincipal.sprite.Height/2);  // sa a l air assez loin
+            mapIntro = new GameObject();
+            mapIntro.estVivant = true;
+            mapIntro.vitesse = 10;
+            mapIntro.sprite = Content.Load<Texture2D>("MapIntro.png");
+            mapIntro.position = mapIntro.sprite.Bounds;
+            mapIntro.position.Offset(mapIntro.sprite.Width/-4, mapIntro.sprite.Height/-4);
+            
+            
+
+            // TODO: use this.Content to load your game content here
+        }
+
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            //---------------------------------------------------------------------------------------------------------------------------------------------------------------------Start update
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W) && Keyboard.GetState().IsKeyUp(Keys.A) && Keyboard.GetState().IsKeyUp(Keys.S) && Keyboard.GetState().IsKeyUp(Keys.D))
+            {
+                mapIntro.position.Y += mapIntro.vitesse;
+                persoPrincipal.etat = GameObjectAnime.Etat.RunHaut;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S) && Keyboard.GetState().IsKeyUp(Keys.A) && Keyboard.GetState().IsKeyUp(Keys.W) && Keyboard.GetState().IsKeyUp(Keys.D))
+            {
+                mapIntro.position.Y -= mapIntro.vitesse;
+                persoPrincipal.etat = GameObjectAnime.Etat.RunBas;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A) && Keyboard.GetState().IsKeyUp(Keys.W) && Keyboard.GetState().IsKeyUp(Keys.S) && Keyboard.GetState().IsKeyUp(Keys.D))
+            {
+                mapIntro.position.X += mapIntro.vitesse;
+                persoPrincipal.etat = GameObjectAnime.Etat.RunGauche;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D) && Keyboard.GetState().IsKeyUp(Keys.A) && Keyboard.GetState().IsKeyUp(Keys.S) && Keyboard.GetState().IsKeyUp(Keys.W))
+            {
+                mapIntro.position.X -= mapIntro.vitesse;
+                persoPrincipal.etat = GameObjectAnime.Etat.RunDroite;
+            }
+            if (Keyboard.GetState().IsKeyUp(Keys.W) && Keyboard.GetState().IsKeyUp(Keys.S) && Keyboard.GetState().IsKeyUp(Keys.A) && Keyboard.GetState().IsKeyUp(Keys.D))
+            {
+                if(persoPrincipal.etat == GameObjectAnime.Etat.RunHaut)
+                {
+                    persoPrincipal.etat = GameObjectAnime.Etat.AttenteHaut;
+                }
+                if (persoPrincipal.etat == GameObjectAnime.Etat.RunBas)
+                {
+                    persoPrincipal.etat = GameObjectAnime.Etat.AttenteBas;
+                }
+                if (persoPrincipal.etat == GameObjectAnime.Etat.RunGauche)
+                {
+                    persoPrincipal.etat = GameObjectAnime.Etat.AttenteGauche;
+                }
+                if (persoPrincipal.etat == GameObjectAnime.Etat.RunDroite)
+                {
+                    persoPrincipal.etat = GameObjectAnime.Etat.AttenteDroite;
+                }
+            }
+
+
+            persoPrincipal.Update(gameTime);
+            base.Update(gameTime);
+            //---------------------------------------------------------------------------------------------------------------------------------------------------------------------End update
+        }
+
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------Start Fonctions
+
+
+        protected void Battle()
+        {
+            battle = true;
+
+
+
+        }
+
+
+
+
+        //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------End fonctions
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            if(intro == true)
+            {
+                spriteBatch.Draw(mapIntro.sprite, mapIntro.position, Color.White);
+            }
+
+            if(battle == true)
+            {
+
+
+            }
+            spriteBatch.Draw(persoPrincipal.sprite, persoPrincipal.position,persoPrincipal.spriteAffiche, Color.White);
+
+
+
+
+
+
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GameObjectAnime.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teka
+{
+    class GameObjectAnime
+    {
+        public Rectangle position;
+        public int vitesse;
+        public Texture2D sprite;
+        public bool estVivant;
+        public Vector2 direction;
+        public Rectangle spriteAffiche; // Le rectangle affiché à l'écran
+
+
+        public enum Etat { AttenteHaut, AttenteBas, AttenteGauche, AttenteDroite, RunDroite, RunGauche, RunHaut, RunBas};
+        public Etat etat;
+
+
+        //competuer qui chargera le sprite afficher
+        private int compteur = 0;
+
+        //gestion tableau sprite
+        int runState = 0; // etat de depart
+        int nbEtatsRun = 4; // nb d etat de run (nb de rectangles)
+
+        public Rectangle[] tabRunDroite =
+        {
+            new Rectangle(0,144,55,72),
+            new Rectangle(55,144,55,72),
+            new Rectangle(110,144,55,72),
+            new Rectangle(165,144,55,72)
+        };
+
+        public Rectangle[] tabRunGauche =
+        {
+            new Rectangle(0,72,55,72),
+            new Rectangle(55,72,55,72),
+            new Rectangle(110,72,55,72),
+            new Rectangle(165,72,55,72)
+        };
+
+        public Rectangle[] tabRunHaut =
+        {
+            new Rectangle(0,216,55,72),
+            new Rectangle(55,216,55,72),
+            new Rectangle(110,216,55,72),
+            new Rectangle(165,216,55,72)
+        };
+
+        public Rectangle[] tabRunBas =
+        {
+            new Rectangle(0,0,55,72),
+            new Rectangle(55,0,55,72),
+            new Rectangle(110,0,55,72),
+            new Rectangle(165,0,55,72)
+        };
+
+        int waitState = 0;
+
+        public Rectangle[] attenteBas =
+        {
+            new Rectangle(0,0,55,72)
+        };
+        public Rectangle[] attenteHaut =
+{
+            new Rectangle(0,216,55,72)
+        };
+        public Rectangle[] attenteGauche =
+{
+            new Rectangle(0,72,55,72)
+        };
+        public Rectangle[] attenteDroite =
+{
+            new Rectangle(0,144,55,72)
+        };
+
+
+
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+            if(etat == Etat.RunHaut)
+            {
+                spriteAffiche = tabRunHaut[runState];
+            }
+            if (etat == Etat.RunBas)
+            {
+                spriteAffiche = tabRunBas[runState];
+            }
+            if (etat == Etat.RunDroite)
+            {
+                spriteAffiche = tabRunDroite[runState];
+            }
+            if (etat == Etat.RunGauche)
+            {
+                spriteAffiche = tabRunGauche[runState];
+            }
+            if (etat == Etat.AttenteHaut)
+            {
+                spriteAffiche = attenteHaut[waitState];
+            }
+            if (etat == Etat.AttenteBas)
+            {
+                spriteAffiche = attenteBas[waitState];
+            }
+            if (etat == Etat.AttenteGauche)
+            {
+                spriteAffiche = attenteGauche[waitState];
+            }
+            if (etat == Etat.AttenteDroite)
+            {
+                spriteAffiche = attenteDroite[waitState];
+            }
+
+
+
+
+            //compteur permettant de generer le changement d images
+            compteur++;
+            if (compteur == 15)//vitesse de defoulement
+            {
+                runState++;
+                if(runState == nbEtatsRun)
+                {
+                    runState = 0;
+                }
+                compteur = 0;
+            }
+
+
+
+
+
+
+        }
+    }
+}
